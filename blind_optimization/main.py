a968f74b0bd807897e3a3430860178d62eeaac37
JeG99 <a00824494@itesm.mx>
2021-08-17 01:52:30 -0500

    fix opmc and add gen overlap, TODO: implement mpmc

--- blind_optimization/main.py
+++ blind_optimization/main.py
@@ -9,8 +9,6 @@ from matplotlib import pyplot as plt
 
 plt.close("all")
 
-experiments = pd.DataFrame()
-
 def generate_solution(min, max, n):
     return [uniform(min, max) for i in range(n)]
 
@@ -29,59 +27,70 @@ def mutate(x_p, delta, min, max):
 def eval(x_i):
     return ackley(x_i, 20, 0.2, 2 * math.pi)
 
-def generate_sons(x_p, n, delta):
-    sons = []
+def generate_children(x_p, n, delta):
+    children = []
     for i in range(n):
-        sons.append(mutate(x_p, delta, -32.768, 32.768))
-    return sons
+        children.append(mutate(x_p, delta, -32.768, 32.768))
+    return children
 
 # Generar padre inicial
 parent = generate_solution(-32.768, 32.768, 2)
 
 print("First parent:", parent)
 
-
 # Un padre varios hijos
-def opms(x_p):
-    intentos = 0
+def opmc(x_p, gen_overlap, iter=5, n_children=10, delta=10):
+    iters = pd.DataFrame()
+    
+    count = 1
     parents = [x_p]
     evaluations = [eval(x_p)]
-    #while(sum(x_p) > 1 or sum(x_p) < -1):
-    #while(x_p[0] < -1 or x_p[0] > 1 or x_p[1] < -1 or x_p[1] > 1):
-    while(intentos < 10):
+
+    while(count < iter):
         # Generar hijos
-        sons = generate_sons(x_p, 30, 10)
-        
+        children = generate_children(x_p, n_children, delta)
+        if gen_overlap:
+            children.append(x_p)
         # Evaluar para selección
         curr_eval = eval(x_p)
-        sons_eval = [eval(son) for son in sons]
+        children_eval = [eval(child) for child in children]
 
         # Selección
-        print(sons[ sons_eval.index(min(sons_eval)) ])
-        if min(sons_eval) < curr_eval:
-            x_p = sons[ sons_eval.index(min(sons_eval)) ]
-            parents.append(x_p)
-            evaluations.append(eval(x_p))
-        
-        intentos += 1
+        try:
+            x_p = children[ children_eval.index(min(children_eval)) ]
+            count += 1
+        except TypeError:
+            continue
+
+        parents.append(x_p)
+        evaluations.append(eval(x_p))
+        if gen_overlap:
+            children.pop()
 
     parents = np.array(parents)
-    experiments['x1'] = parents[:,0]
-    experiments['x2'] = parents[:,1]
-    experiments['f(x)'] = evaluations
-    experiments.plot()
-    plt.show()
+    iters['iterations'] = range(0, iter)
+    iters['x1'] = parents[:, 0]
+    iters['x2'] = parents[:, 1]
+    iters['f(x)'] = evaluations
     
-    return x_p
+    return x_p, iters
 
-# Varios padres varios hijos
-def mpms():
-    pass
+best_parents = []
+experiments = []
+for i in range(1):
+    best_parent, iterations = opms(parent, False, 100, 100, 100)
+    best_parents.append(best_parent)
+    experiments.append(iterations)
 
-print(eval([1, 1]))
+results = pd.concat(experiments, ignore_index=True)
+#with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+print(results)
+#results.plot()
 
-opms(parent)
-print(experiments)
-#plt.plot([ackley([x], 20, 0.2, 2 * math.pi) for x in range(-32, 32)])
-#plt.show()
+plt.figure()
+plt.plot(results.index, results['x1'])
+plt.plot(results.index, results['x2'])
+plt.show()
+
+
 
--- blind_optimization/utils.py
+++ blind_optimization/utils.py
@@ -8,8 +8,8 @@ def ackley(sol, a, b, c):
     for x in sol:
         #if x == 0:
         #    return 
-        sum_1 = sum_1 + x * x
-        sum_2 = sum_2 + math.cos(c * x)
+        sum_1 += x * x
+        sum_2 += math.cos(c * x)
 
     try:
         output = -a * math.e ** ( (sum_1/d) ** (-1/b) ) - math.e ** (sum_2/d) + a + math.e
